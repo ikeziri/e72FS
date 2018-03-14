88c3fb861ed465fa711b01966e86c966e89c39d1
Pandre <pandre.barbosa@gmail.com>
2018-03-14 18:01:41 -0300

    Coloquei o comando de retirar a navbar do login no arquivo login.js

--- projetoPrevia/App.js
+++ projetoPrevia/App.js
@@ -23,9 +23,7 @@ function startApp() {
   Navigation.startSingleScreenApp({
     screen: {
       screen: 'example.Login', // unique ID registered with Navigation.registerScreen
-      navigatorStyle: {
-        navBarHidden: true,
-      }, // override the navigator style for the screen, see "Styling the navigator" below (optional)
+      navigatorStyle: {}, // override the navigator style for the screen, see "Styling the navigator" below (optional)
       navigatorButtons: {} // override the nav buttons for the screen, see "Adding buttons to the navigator" below (optional)
     },
     drawer: {
