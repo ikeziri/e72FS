bee7d2a4e954917ca3fd3bcedb81472bbf7c9000
Pandre <pandre.barbosa@gmail.com>
2018-03-14 21:02:41 -0300

    estilização completa da tela inicial, a de login

--- projetoPrevia/screens/Login.js
+++ projetoPrevia/screens/Login.js
@@ -6,12 +6,17 @@ import {
   Alert,
   Image,
   Button,
+  Platform,
+  TextInput,
+  Dimensions,
   StyleSheet,
   AppRegistry,
   AsyncStorage,
   TouchableOpacity,
   ActivityIndicator,
 } from 'react-native';
+
+var {height, width} = Dimensions.get('window');
 
 export default class Login extends Component {
   static navigatorStyle = {
@@ -23,6 +28,7 @@ export default class Login extends Component {
       isLoading: true,
       text: ' ',
       value: 0,
+      placeHolderEmail: 'Endereço de email',
     }
     this.props.navigator.setDrawerEnabled({
       side: 'left', // the side of the drawer since you can have two, 'left' / 'right'
@@ -53,7 +59,7 @@ export default class Login extends Component {
     }
   };
 
-  onPressBtn= async () => {   
+  _onPressButton = async () => {   
     try {
       await AsyncStorage.setItem('Login', 'logado');
       this._loadInitialState().done();
@@ -72,12 +78,42 @@ export default class Login extends Component {
     }
     return (
       <View style={styles.container}>
-        <Image
-        source={require('../images/logo-descomplica.png')}
-         />
-        <TouchableOpacity style={styles.button} >
-          <Text style={styles.textButton} onPress={this.onPressBtn} > Fazer login </Text>
-        </TouchableOpacity>
+
+        <View style={styles.containerLogo}>
+        <Image style={styles.logoEntrada}
+                 source={require('../images/logo-descomplica.png')} />
+        </View>
+
+        <View style={styles.linha}>
+          <View style={styles.hrComTexto} />
+          <Text style={styles.textoHr}>Entrar com email</Text>
+          <View style={styles.hrComTexto} />
+        </View>
+
+         <View style={styles.containerFields}>
+          <TextInput style={styles.campoTextoSuperior}
+            value={this.state.placeHolderEmail}
+            onChangeText={(placeHolderEmail) => this.setState({placeHolderEmail})} />
+          <TextInput style={styles.campoTextoInferior}
+            secureTextEntry={true}
+            value="abc"  />
+         </View>
+
+
+        <Text style={styles.linkLembrarSenha}>Esqueci a senha</Text>
+
+        <View style={styles.linha}>
+          <View style={styles.hr} />
+        </View>
+
+        <View style={styles.containerButtons}>
+          <TouchableOpacity style={styles.buttonLogin} >
+            <Text style={styles.textButton} onPress={this._onPressButton} > Login </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonInscrevaSe} >
+            <Text style={styles.textButton} onPress={this._onPressButton} > Inscreva-se </Text>
+          </TouchableOpacity>      
+        </View>
       </View>
     );
   };
@@ -86,19 +122,106 @@ export default class Login extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#0A263C' //Fundo principal da tela
   },
-  button: {
+  containerButtons:{
+    flexDirection: 'row',
+    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#0A263C',
+  },
+  containerFields:{
+    justifyContent: 'center',
+  },
+  containerLogo:{
+    alignItems: 'center',
+  },
+  buttonLogin: {
+    alignItems: 'center',
+    backgroundColor: '#12477e',
+    marginLeft: 10,
+    marginRight: 10,
+    borderRadius: 5,
     padding: 10,
-    borderRadius: 20,
-    marginTop: 10,
+  },
+  buttonInscrevaSe: {
+    alignItems: 'center',
+    backgroundColor: '#12455e',
+    borderRadius: 5,
+    marginLeft: 20,
+    marginRight: 20,
+    padding: 10,
   },
   textButton: {
-    fontSize: 25,
+    fontSize: 20,
     color: '#fff',
   },
+  linha:{
+    flexDirection: 'row',
+    paddingTop: 40,
+    paddingBottom: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textoHr:{
+    fontSize: 13,
+    color: '#fff',
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  logoEntrada: {
+    width: 130,
+    height: 70,
+    marginTop: (Platform.OS === 'ios' ? 40 : 20 ),
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  hr:{
+    borderBottomColor: '#12466e',
+    borderBottomWidth: 1,
+    width: width - 40,
+  },
+  hrComTexto:{
+    borderBottomColor: '#12466e',
+    borderBottomWidth: 1,
+    width: width - 290,
+  },
+  campoTextoSuperior: {
+    height: 60,
+    borderColor: '#fff',
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    fontSize: 15,
+    marginLeft: 15,
+    marginRight: 15,
+  },
+  campoTextoInferior: {
+    height: 60,
+    borderColor: '#fff',
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0,
+    fontSize: 15,
+    marginLeft: 15,
+    marginRight: 15,
+  },
+  inscrevaSe:{
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  linkLembrarSenha: {
+    color: '#fff',
+    paddingTop: 10,
+    paddingBottom: 30,
+    marginLeft: 15,
+  },
+  normal:{
+    color: '#fff',
+  }
 });
